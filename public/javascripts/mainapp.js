a9b7646008c7978f25626bbe5b40f04f53e4dc32
Artem Arakcheev <artem.ft@gmail.com>
2014-12-14 23:43:00 +0300

    add datepicket

--- public/javascripts/mainapp.js
+++ public/javascripts/mainapp.js
@@ -1723,7 +1723,7 @@
 }).call(this);
 
 (function () {
-    angular.module('app.wbox', ['app.wbox.services', 'app.wbox.controllers'])
+    angular.module('app.wbox', ['app.wbox.services', 'app.wbox.controllers', 'textAngular'])
 }).call(this);
 
 
@@ -1977,6 +1977,40 @@
             };
         }]).controller("WboxDocEdit", ['$scope', '$location', function ($scope, $location) {
             var id = $location.url()['id'];
+
+            $scope.today = function () {
+                $scope.dt = new Date();
+            };
+            $scope.today();
+
+            $scope.clear = function () {
+                $scope.dt = null;
+            };
+
+            // Disable weekend selection
+            $scope.disabled = function (date, mode) {
+                return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+            };
+
+            $scope.toggleMin = function () {
+                $scope.minDate = $scope.minDate ? null : new Date();
+            };
+            $scope.toggleMin();
+
+            $scope.open = function ($event) {
+                $event.preventDefault();
+                $event.stopPropagation();
+
+                $scope.opened = true;
+            };
+
+            $scope.dateOptions = {
+                formatYear: 'yy',
+                startingDay: 1
+            };
+
+            $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+            $scope.format = $scope.formats[0];
 
         }]).controller("WboxDocView", ['$scope', '$location', function ($scope, $location) {
             var id = $location.url()['id'];
